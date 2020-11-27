e0d9af57b2f8d2656971a9ff34dc8ab59b1f5a8b
Arrandes <70968911+Arrandes@users.noreply.github.com>
2020-11-27 19:36:05 +0300

    Add files via upload

---
+++ SwappingVariables/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwappingVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Выполнил: Скворцов Александр
+             * Написать программу обмена значениями двух переменных.
+             * а) с использованием третьей переменной;
+             * б) без использования третьей переменной.
+             */
+
+            int a, b, c;
+            Console.Write("Введите значение для переменной a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение для переменной b: ");
+            b = int.Parse(Console.ReadLine());
+
+            //С использованием третьей переменной;
+            //c = a;
+            //a = b;
+            //b = c;
+
+            //Без использования третьей переменной
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("Значение переменной a: " + a);
+            Console.WriteLine("Значение переменной b: " + b);
+        }
+    }
+}
